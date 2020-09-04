e0950d1ad54877f3ab52f4075016096cdfd23988
Deborah Mendes <deb.mendes.9@gmail.com>
2020-09-04 09:59:54 +0530

    Set Loader to all display pages

--- savish/src/components/Display/DisplayPaint.js
+++ savish/src/components/Display/DisplayPaint.js
@@ -1,52 +1,51 @@
-import React, { Component} from 'react'
-import axios from 'axios'
-import styles from './Display.module.css';
-import PaintCard from './PaintCard';
+import React, { Component } from "react";
+import axios from "axios";
+import styles from "./Display.module.css";
+import PaintCard from "./PaintCard";
+import Loader from "../Loader/Loader";
 
-const Paint=props=>{
-        return(         
-            <PaintCard post={props.paint}/>
-        )   
-}
+const Paint = (props) => {
+  return <PaintCard post={props.paint} />;
+};
 
 class DisplayPaint extends Component {
-    constructor(props) {
-        super(props);
-        this.state={
-            paint:[]
-        }
-        this.state.paintlist=this.paintlist()
+  constructor(props) {
+    super(props);
+    this.state = {
+      paint: [],
+      loading: true,
+    };
+    this.state.paintlist = this.paintlist();
+  }
+  async componentDidMount() {
+    axios
+      .get("http://localhost:3000/paint/")
+      .then((response) => {
+        this.setState({
+          paint: response.data,
+          loading: false,
+        });
+        console.log(response.data);
+      })
+      .catch((err) => console.log("khada haii khada hai khada haii"));
+  }
+
+  paintlist() {
+    return this.state.paint.map((currentpaint) => {
+      return <Paint paint={currentpaint} key={currentpaint._id} />;
+    });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <Loader />;
     }
-    async componentDidMount() 
-        {
-            
-            axios.get('http://localhost:3000/paint/')
-                .then(response => {
-                    this.setState({
-                         
-                         paint: response.data
-                    })
-                    console.log(response.data)
-                    
-                })
-                .catch(err => console.log('khada haii khada hai khada haii'))
-        }
-    
-    paintlist(){
-        return this.state.paint.map(currentpaint => {
-            console.log(currentpaint)
-            return <Paint paint={currentpaint} key={currentpaint._id} />
-        })
-    }
-    render() {
-        return (
-            <div className={styles.top} >
-                <div className={styles.container}>
-                    {this.paintlist()}
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div className={styles.top}>
+        <div className={styles.container}>{this.paintlist()}</div>
+      </div>
+    );
+  }
 }
 
-export default DisplayPaint
+export default DisplayPaint;
--- savish/src/components/Display/DisplayPoem.js
+++ savish/src/components/Display/DisplayPoem.js
@@ -1,24 +1,27 @@
-import React, { useState,useEffect } from 'react'
-import axios from 'axios'
-import Display from './Display';
-const Poem = () => {
-    const [poems,setPoems] = useState([])
-    useEffect(() => {
-        const getData = async () => {
-            await axios.get('http://localhost:3000/poem/')
-             .then(response => {
-                setPoems(response.data)
-             })
-             .catch(err => console.log(err))
-        }
-        getData();
-    })
-    
-    return (
-        <div>
-           {poems &&(<Display posts={poems}/>)} 
-        </div>              
-    )
-}
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Loader from "../Loader/Loader";
+import Display from "./Display";
 
-export default Poem
+const Poem = () => {
+  const [poems, setPoems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const getData = async () => {
+      await axios
+        .get("http://localhost:3000/poem/")
+        .then((response) => {
+          setPoems(response.data);
+          setLoading(false);
+        })
+        .catch((err) => console.log(err));
+    };
+    getData();
+  });
+  if (loading) {
+    return <Loader />;
+  }
+  return <div>{poems && <Display posts={poems} />}</div>;
+};
+
+export default Poem;
